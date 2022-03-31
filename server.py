4d48814d2d44c688c55b64b55a3fc5556bb9e7f4
natallia10 <kuzmina10n@gmail.com>
2022-03-31 12:57:03 +0300

    first version

---
+++ server.py
@@ -0,0 +1,34 @@
+#server
+import socket
+import codecs
+
+PORT = 65535
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    s.bind(('', PORT))
+    clients = set()
+    print('Listeing on port', PORT)
+
+    while True:
+        try:
+            data, addr_info = s.recvfrom(4096)
+            
+            if addr_info not in clients:
+                if len(clients) == 5:
+                    continue
+                print('New client:', addr_info)
+                clients.add(addr_info)   
+                         
+            print('Client [%s, %r]:' % addr_info, data)
+
+            for client_addr in clients:
+                s.sendto(data, client_addr)
+                
+        except KeyboardInterrupt:
+            break
+    
+    s.close()
+
+if __name__== '__main__':
+    main()  
